96ab9f38c5a1f082396ea4c3b6b794b71f882173
adz5a <antoine.d.zimmermann@gmail.com>
2017-10-02 15:20:45 +0200

    jest bug before eject

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe("one test", () => {
+
+    it.skip("should be skipped", () => {
+        expect.hasAssertions();
+
+
+    }); 
+
+    it("should not be skipped", () => {
+        expect.hasAssertions();
+
+        expect(true).toBe(true);
+    }); 
 });
+
+
+describe.skip("1 - all tests should be skipped", () => {
+
+    // both should be skipped
+    it.skip("should be skipped", () => {
+        expect.hasAssertions();
+
+
+    }); 
+
+    it("should not be skipped", () => {
+        expect.hasAssertions();
+
+        expect(true).toBe(true);
+    }); 
+});
+
+describe.skip("2 - all tests should be skipped", () => {
+
+    // both should be skipped
+    it("should not be skipped", () => {
+
+        expect(true).toBe(true);
+    }); 
+
+    it.skip("should be skipped", () => {
+
+
+    }); 
+});
+
+describe("3 - all tests should be skipped", () => {
+
+    // both should be skipped
+    it("should not be skipped", () => {
+
+        expect(true).toBe(true);
+    }); 
+
+    it.skip("should be skipped", () => {
+
+        expect.hasAssertions();
+    }); 
+});
+
